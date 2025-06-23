ab0dcb5e01f1041edbcab8121cc21e9d2cdb48d4
edilson <edilsondiasgomes@gmail.com>
2025-06-23 15:19:47 -0300

    implementando o zxing scanner

---
+++ src/app/zxing-scanner/zxing-scanner.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarcodeFormat } from '@zxing/library';
+
+
+@Component({
+  selector: 'app-zxing-scanner',
+  standalone: true,
+  imports: [ZXingScannerModule],
+  templateUrl: './zxing-scanner.component.html',
+  styleUrl: './zxing-scanner.component.scss'
+})
+
+export class ZxingScannerComponent {
+
+  allowedFormats = [
+    BarcodeFormat.QR_CODE,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.ITF
+  ];
+  result: any
+
+  scanCompleteHandler(result: any){
+    this.result = result
+    console.log(result);
+    
+  }
+
+}
