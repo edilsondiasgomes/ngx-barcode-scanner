44623be9b8a1473265014790f3f2aaa2f23f89e3
edilson <edilsondiasgomes@gmail.com>
2025-06-23 15:19:58 -0300

    validando visualizacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component, } from '@angular/core';
 import { BarcodeScannerComponent } from './barcode-scanner/barcode-scanner.component';
+import { ZxingScannerComponent } from './zxing-scanner/zxing-scanner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BarcodeScannerComponent],
+  imports: [BarcodeScannerComponent, ZxingScannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
+
+  quagga2=true;
   
+
+  switchScanner(){
+    this.quagga2 = !this.quagga2;
+  }
 
 }
